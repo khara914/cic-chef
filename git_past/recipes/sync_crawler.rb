4d637e020e183068ca43ced56b36135da0a773a5
Momoha Oka <momoha.oka@ctc-g.co.jp>
2015-04-15 18:55:14 +0900

    delete old

--- git_past/recipes/sync_crawler.rb
+++
@@ -1,5 +0,0 @@
-git "/cic-crawler" do
-   repository "https://github.com/khara914/cic-crawler.git"
-   revision "master"
-   action :sync
-end
--- git_past/recipes/sync_tech-info.rb
+++
@@ -1,5 +0,0 @@
-git "/var/www/html" do
-   repository "https://github.com/khara914/cic-tech-info.git"
-   revision "master"
-   action :sync
-end
