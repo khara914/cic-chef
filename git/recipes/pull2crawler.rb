b46a44e8aad09ee32a0134f609469a345a120101
Momoha Oka <momoha.oka@ctc-g.co.jp>
2015-04-15 13:57:34 +0900

    change pulled directory

--- git/recipes/pull2crawler.rb
+++ git/recipes/pull2crawler.rb
@@ -2,7 +2,7 @@ package 'git' do
    action :install
 end
 
-git "/var/www/html" do
+git "/usr/src" do
    repository "https://github.com/khara914/cic-crawler.git"
    revision "master"
    action :sync
