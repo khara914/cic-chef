09bb3c51f72b35ab4a8c9ee690cb10203794e0cd
Momoha Oka <momoha.oka@ctc-g.co.jp>
2015-04-15 17:28:42 +0900

    replace

---
+++ git_past/recipes/sync_crawler.rb
@@ -0,0 +1,5 @@
+git "/cic-crawler" do
+   repository "https://github.com/khara914/cic-crawler.git"
+   revision "master"
+   action :sync
+end
---
+++ git_past/recipes/sync_tech-info.rb
@@ -0,0 +1,5 @@
+git "/var/www/html" do
+   repository "https://github.com/khara914/cic-tech-info.git"
+   revision "master"
+   action :sync
+end
